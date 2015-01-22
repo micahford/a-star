355e72f0013ec82885ea99227389fb6cb57acb63
Micah Brown <micahbrown93@gmail.com>
2015-01-22 18:02:16 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AStar a = new AStar();
+		System.out.println(a);
+		a.moveSquare(4, a);
+		System.out.println("");
+		System.out.println(a);
+	}
+
+}
