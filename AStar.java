17b1aadaee91216d064bc839e8ecca4e08d2208d
Micah Brown <micahbrown93@gmail.com>
2015-01-26 21:59:20 -0500

    Update AStar.java
    -made moveSquare method void and removed the parameter of the board. this way, it acts on the instance of the AStar.
    -inserted isSolvable method which checks whether a given board is solvable.

--- AStar.java
+++ AStar.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 
 public class AStar {
@@ -14,7 +15,7 @@ public class AStar {
 
 		List<Integer> l = new ArrayList<Integer>();
 		for(int i = 0; i<size*size; i ++){l.add(i);}
-		Collections.shuffle(l);
+		Collections.shuffle(l, new Random(4));
 		
 		for(int i = 0; i< size; i++){
 			for(int j = 0; j < size; j++){
@@ -24,25 +25,62 @@ public class AStar {
 	}
 	
 	//swaps the number n with the blank space (number 0)
-	public AStar moveSquare(int n, AStar a){
+	public void moveSquare(int n){
 		int rowNumber = 0; int colNumber = 0;
 		int rowZero = 0; int colZero = 0;
 		for(int i = 0; i < size; i++){
 			for (int j = 0; j < size; j++){
-				if(a.board[i][j]==0){
+				if(board[i][j]==0){
 					rowZero=i;
 					colZero=j;
 				}
-				else if (a.board[i][j]==n){
+				else if (board[i][j]==n){
 					rowNumber = i;
 					colNumber = j;
 				}
 			}
 		}
-		a.board[rowZero][colZero] = n;
-		a.board[rowNumber][colNumber] = 0;
+		board[rowZero][colZero] = n;
+		board[rowNumber][colNumber] = 0;
 		
-		return a;
+		//return a;
+	}
+	
+	//returns boolean whether the puzzle is solvable
+	public boolean isSolvable() {
+		int[] boardInOrder = this.orderBoard();
+		int N = 0;
+		int e = 0;
+		for (int i = 0; i < boardInOrder.length; i++){
+			int n = 0;
+			if ((boardInOrder[i]!=0)||(boardInOrder[i]!=1)){
+				for(int j = i; j<boardInOrder.length;j++){
+					if(boardInOrder[i]>boardInOrder[j]){
+						if(boardInOrder[j]!= 0) n++;
+					}
+				}
+				System.out.print(n + " ");
+				N = N + n;
+
+			}
+			else if (boardInOrder[i]==0) {e = i+1;}
+
+		}
+		if((N+e)%2==0) return true;
+		else return false;
+	}
+	
+	//takes the current board (a 2D array and returns a 1D array of the numbers written in order)
+	private int[] orderBoard() {
+		int[] boardInOrder = new int[9];
+		int k = 0;
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				boardInOrder[k] = board[i][j];
+				k++;
+			}
+		}
+		return boardInOrder;
 	}
 	
 	//prints off the current board.
@@ -58,4 +96,3 @@ public class AStar {
 		return toReturn;
 	}
 }
-
