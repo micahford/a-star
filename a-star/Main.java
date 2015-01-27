44e90c6fcc3d4198e4f53e8d5126902329cc26d9
m1cah <micahbrown93@gmail.com>
2015-01-27 11:06:25 -0500

    test comment command line

---
+++ a-star/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AStar a = new AStar();
+		System.out.println(a);
+		a.moveSquare(4);
+		System.out.println("");
+		System.out.println(a);
+		System.out.println(a.isSolvable());
+	}
+
+}
