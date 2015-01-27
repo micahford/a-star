723da0e85bebd9adc20a0c3090732c829d4cb173
fordholland <foholland@davidson.edu>
2015-01-27 18:08:00 -0500

    Update AStar.java
    Added isOutOfBounds, blankLoc, and unfinished possMoves

--- AStar.java
+++ AStar.java
@@ -2,39 +2,32 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-
-
 public class AStar {
 	final static int size = 3;
-	
 	private int[][] board;
-	
-	
 	public AStar(){
 		board = new int[size][size];
-
 		List<Integer> l = new ArrayList<Integer>();
 		for(int i = 0; i<size*size; i ++){l.add(i);}
 		Collections.shuffle(l, new Random(4));
-		
 		for(int i = 0; i< size; i++){
 			for(int j = 0; j < size; j++){
 				board[i][j] = l.get(i*size+j);
 			}
 		}
 	}
-	
 	//swaps the number n with the blank space (number 0)
-	public void moveSquare(int n){
+	public AStar moveSquare(int n, AStar a){
 		int rowNumber = 0; int colNumber = 0;
 		int rowZero = 0; int colZero = 0;
-		for(int i = 0; i < size; i++){
-			for (int j = 0; j < size; j++){
-				if(board[i][j]==0){
+		
+		for(int i = 0; i < a.size; i++){
+			for (int j = 0; j < a.size; j++){
+				if(a.board[i][j]==0){
 					rowZero=i;
 					colZero=j;
 				}
-				else if (board[i][j]==n){
+				else if (a.board[i][j]==n){
 					rowNumber = i;
 					colNumber = j;
 				}
@@ -42,8 +35,48 @@ public class AStar {
 		}
 		board[rowZero][colZero] = n;
 		board[rowNumber][colNumber] = 0;
+		return a;
+	}
+	
+	//returns list containing coordinates of the blank space
+	private int[] blankLoc(AStar a){
+		int[] location = null;
+		for (int i=0; i<a.size; i++){
+			for (int j=0; j<a.size; j++){
+				if (a.board[i][j]==0){
+					location[0]=i;
+					location[1]=j;
+				}
+			}
+		}
+		return location;
+	}
+	
+	
+	
+	//generates successor states
+	public List<AStar> possMoves(AStar a){
+		List<AStar> l= new ArrayList<AStar>();
+		int [] location = a.blankLoc(a);
+		int r=location[0];
+		int c=location[1];
 		
-		//return a;
+		for (int i=0; i<4; i++){
+			
+		}
+		return l;
+	}
+	
+	//checks if a location is out of bounds
+	private boolean isOutOfBounds(int r, int c, AStar a){
+		boolean isOutOfBounds=false;
+		if ((r+1>a.size) || (r-1<a.size)){
+			isOutOfBounds=true;
+		}
+		else if ((c+1>a.size) || (c-1<a.size)){
+			isOutOfBounds=true;
+		}
+		return isOutOfBounds;
 	}
 	
 	//returns boolean whether the puzzle is solvable
@@ -61,15 +94,12 @@ public class AStar {
 				}
 				System.out.print(n + " ");
 				N = N + n;
-
 			}
 			else if (boardInOrder[i]==0) {e = i+1;}
-
 		}
 		if((N+e)%2==0) return true;
 		else return false;
 	}
-	
 	//takes the current board (a 2D array and returns a 1D array of the numbers written in order)
 	private int[] orderBoard() {
 		int[] boardInOrder = new int[9];
@@ -82,10 +112,9 @@ public class AStar {
 		}
 		return boardInOrder;
 	}
-	
 	//prints off the current board.
- 	public String toString(){
- 		String toReturn = "";
+	public String toString(){
+		String toReturn = "";
 		for(int i = 0; i < size; i++){
 			for(int j = 0; j < size; j++){
 				if(board[i][j]==0) toReturn += " ";
